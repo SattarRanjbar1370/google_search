9a99b95f907cc115bd5025e72b3b5122eac8fca5
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-20 15:44:39 +0330

    root/app.js >>>>>> debuging

--- root/app.js
+++ root/app.js
@@ -3,7 +3,7 @@ let divheadLeft, divheadRight, divbox_base1, divbox_base2, divbox_base3, boxMod;
 // head layout in html document
 
 divheadLeft = document.getElementById('headleft');
-divheadLeft.innerHTML = '<ul class="listLeftHead" id="listLeftHead"><li><a href="#">About</a></li><a href="#">Stor</a><li></li></ul>';
+divheadLeft.innerHTML = '<ul class="listLeftHead" id="listLeftHead"><li><a href="#">About</a></li><li><a href="#">Stor</a></li></ul>';
 boxMod = '<div class="boxMod box_R_1" id="box_R_1"></div><div class="boxMod box_R_2" id="box_R_2"></div>';
 divheadRight = document.getElementById('headRight');
 divheadRight.innerHTML = boxMod;
